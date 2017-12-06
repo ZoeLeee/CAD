8381c86acb0caecdeef9d9cdd5e24275208e3c4f
zoe <lizhou1021@qq.com>
2017-12-06 17:29:13 +0800

    '修改提示块为无状态组件'

--- src/components/HintBlock/HintBlock.tsx
+++ src/components/HintBlock/HintBlock.tsx
@@ -3,54 +3,18 @@ import './HintBlock.css';
 
 export interface HintBlockProps
 {
+
 }
 
-export interface HintBlockState 
+
+let HintBlock = (props: HintBlockProps) =>
 {
-    pos: React.CSSProperties; //提示块位置
-    isShow: React.CSSProperties;
-}
-
-export default class HintBlock extends React.Component<HintBlockProps, HintBlockState>
-{
-    public state: HintBlockState;
-    constructor(props: HintBlockProps)
-    {
-        super(props);
-
-        this.state =
-            {
-                isShow: {},
-                pos: { display: 'none', left: 0, top: 0 }
-            }
-    }
-    public handleshowBlock()
-    {
-        document.addEventListener('click', (e) =>
-        {
-
-            this.setState({ pos: { display: 'block', left: e.clientX + 20 + 'px', top: e.clientY + 20 + 'px' } });
-            document.addEventListener('mousemove', (e) =>
-            {
-                this.setState({ pos: { display: 'block', left: e.clientX + 20 + 'px', top: e.clientY + 20 + 'px' } });
-            })
-
-        })
-    }
-    componentDidMount()
-    {
-
-
-
-    }
-    render() 
-    {
-        return (
-            <div id="hint-block" style={this.state.pos}>
-                <label></label>
-                <input type="text" autoFocus />
-                <label></label>
-            </div>
-        );
-    }
-}
+    return (
+        <div id="hint-block">
+            <label></label>
+            <input type="text" autoFocus />
+            <label></label>
+        </div>
+    );
+};
+export default HintBlock;
